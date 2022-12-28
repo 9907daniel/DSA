143775d3a349444dea3a36edca5f3d44ea37adca
system76 <jeonghoo.shin@rapyuta-robotics.comclear>
2022-12-28 14:53:45 +0900

    update

--- Day1/pascal_triangle.py
+++ Day1/pascal_triangle.py
@@ -1,6 +1,43 @@
-class Solution {
-public:
-    vector<vector<int>> generate(int numRows) {
+
+
+
+class Solution:
+    def generate(numRows: int):
+        initial_list = [1]
+        empty_list = []
         
-    }
-};
+        n = 0
+        m = 0
+        while n < numRows: 
+            for i in initial_list:
+                try:
+                    if initial_list[m]:
+                        empty_list[m] = initial_list[m] + initial_list[m+1]
+                        m += 1
+                except:
+                    empty_list[m] = initial_list[m]
+                    m+=1
+            n += 1
+            
+    
+    print(generate(1))
+        
+        # n = 0
+        # while n < numRows:
+            
+        #     # empt_dic[round(n/2)] == 
+        #     for i in empt_dic:
+        #         empt_dic[n]
+            
+        #     n += 1 
+        
+
+
+# Input: numRows = 5
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+#    1
+#   1 1
+#  1 2 1
+# 1 3 3 1
+#1 4 6 4 1
