36095265db292348d64024c76ffe38b534944bac
9907daniel <daniel.9907@Hotmail.com>
2022-12-29 01:00:28 +0900

    solved Day1

--- Day1/pascal_triangle.py
+++ Day1/pascal_triangle.py
@@ -1,38 +1,3 @@
-
-
-
-class Solution:
-    def generate(numRows: int):
-        initial_list = [1]
-        empty_list = []
-        
-        n = 0
-        m = 0
-        while n < numRows: 
-            for i in initial_list:
-                try:
-                    if initial_list[m]:
-                        empty_list[m] = initial_list[m] + initial_list[m+1]
-                        m += 1
-                except:
-                    empty_list[m] = initial_list[m]
-                    m+=1
-            n += 1
-            
-    
-    print(generate(1))
-        
-        # n = 0
-        # while n < numRows:
-            
-        #     # empt_dic[round(n/2)] == 
-        #     for i in empt_dic:
-        #         empt_dic[n]
-            
-        #     n += 1 
-        
-
-
 # Input: numRows = 5
 # Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
@@ -41,3 +6,22 @@ class Solution:
 #  1 2 1
 # 1 3 3 1
 #1 4 6 4 1
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        answer = []
+        for i in range(numRows):
+            tmp = []
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    tmp.append(1)
+                else:
+                    tmp.append(answer[i-1][j-1] + answer[i-1][j])
+            answer.append(tmp)
+        return answer
+
+
+# Time Complexity : O(n)
+# Take : utilize previous list[i-1] to add
+
