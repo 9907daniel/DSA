932940d183ae381e147c8d06bcafe7ba7a2a4b3b
9907daniel <daniel.9907@hotmail.com>
2023-04-10 13:53:18 +0900

    4.10

--- bekjun/BFS/2606.py
+++ bekjun/BFS/2606.py
@@ -2,24 +2,45 @@ from collections import deque
 
 n = int(input())
 m = int(input())
-l = []
-visited = [False]*n
+graph = []
+visited = [False]*(m+1)
 
 for a in range(m):
-    l.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
+
+
+def bfs(i,j, a):
+    # count = 0
+    q = deque()
+    q.append((i,j))
+    visited[a] = True
+    checked = []
+    
+    while q:
+        x, y = q.popleft()
+        
+        for a in range(m):
+            if visited[a] != True:
+                if graph[a][0] == x or graph[a][1] == y or graph[a][1] == x or graph[a][0] == y:
+                    if graph[a][0] not in checked:
+                        checked.append(graph[a][0])
+                    if graph[a][1] not in checked:
+                        checked.append(graph[a][1])
+                    
+                    q.append((graph[a][0],graph[a][1]))
+                    visited[a] = True
+
+    print(checked)
+    return len(checked)-1
 
 count = 0
-
-def bfs(l,start,visited, count):
-    queue = deque([start])
-    visited[start] = True
+for a in range(m):
+    if graph[a][0] == 1 or graph[a][1] == 1:
+        print(bfs(graph[a][0],graph[a][1], a))
+        count += 1
+        break
     
-    while queue:
-        x = queue.popleft()
-        for i in l[x]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-                count += 1
-                
-bfs(l,0,visited, 0)
+if count == 0:
+    print(count)
+
+        
--- bekjun/sort/1764.py
+++ bekjun/sort/1764.py
@@ -1,29 +1,35 @@
+# https://www.acmicpc.net/problem/1764
+# 듣도 보도 못한 사람 수 찾기
+
 n, m = map(int, input().split())
-
-n_list = []
-m_list = []
-
-for a in range(n):
-    n_list.append(input())
-
-for a in range(m):
-    m_list.append(input())
-
-count = 0
-tmp = []
-
-result = sorted(m_list&n_list)
+n_list = set()
+m_list = set()
+for _ in range(n):
+    n_list.add(input())
+for _ in range(m):
+    m_list.add(input())
 
 
-# n_list.sort()
-# m_list.sort()
+tmp = sorted(list(n_list&m_list))
 
-# for a in range(len(n_list)):
-# for a in m_list:
-#     if a in n_list:
-#         count += 1
+print(len(tmp))
+for a in tmp:
+    print(a)
+
+# count = 0
+# tmp = []
+
+# for a in n_list:
+#     if a in m_list:
 #         tmp.append(a)
-
+#         count += 1
+        
 # print(count)
-# for a in tmp:
-#     print(a)
+# tmp.sort()
+
+# for a in range(len(tmp)):
+#     print(tmp[a])
+
+
+
+
