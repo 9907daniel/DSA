007c88f159e383b17a0ed100ec8a68b035ddaf26
9907daniel <daniel.9907@hotmail.com>
2023-05-02 15:57:55 +0900

    5.2

--- bekjun/Greedy/1339.py
+++ bekjun/Greedy/1339.py
@@ -5,21 +5,19 @@ d = [0]*100
 for a in range(n):
     l.append(input())
     
-for a in range(len(l)):
-    for b in range(len(l[a])):
-        d[ord(l[a][b])] += 1
+
+if len(l[0]) > len(l[1]):
     
-print(d)
     
-# for a in range(len(l[1])):
-#     d[ord(l[0][a])] += 1
     
-# d.sort()
-# d.reverse()
-# # for a in range(10):
-# #     for b in range(d[a]):
-# #         d[a][b] = d[a][b]*d[a][b]
-        
+    
+    
+    
+    
+# for a in range(len(l)):
+#     for b in range(len(l[a])):
+#         d[ord(l[a][b])] += 1
+    
 # print(d)
-# # print(sum(d))
+
     
